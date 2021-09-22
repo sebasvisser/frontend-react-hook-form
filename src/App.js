63ac52cafcb25c2e483cd6b70e95f2a14e71f950
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-09-22 23:16:43 +0200

    Opdracht 1 compleet af. Inclusief onblur validatie

--- src/App.js
+++ src/App.js
@@ -3,51 +3,110 @@ import { useForm } from 'react-hook-form';
 import './App.css';
 
 export default function App() {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-    const chosenFreq = watch('Bezorgfrequentie');
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({mode: "onBlur"});
 
-
-    console.log(errors);
-
-
+    function onSubmit(data){
+        console.log(data);
+    }
+    const chosenFreq = watch('bezorgfrequentie');
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder="Voornaam"
-                   {...register("voornaam",
-                       {required: true})}
-            />
-            <input type="text" placeholder="Achternaam"
-                   {...register("achternaam",
-                       {required: true})}
-            />
-            <input type="number" placeholder="Leeftijd"
-                   {...register("leeftijd",
-                       {required: true,
-                           min: {
-                               value: 18,
-                               message: "Er mogen maximaal 50 karakters gebruikt worden",
-                           },
+            <label>
+                Voornaam:
+                <input type="text" placeholder="Voornaam"
+                       {...register("voornaam",
+                           {required: "Voornaam mag niet leeg zijn"})}
+                    onBlur={handleSubmit}
+                />
+            </label>
+            {errors.voornaam && <p>{errors.voornaam.message}</p>}
+
+
+            <label>
+                Achternaam:
+                <input type="text" placeholder="Achternaam"
+                       {...register("achternaam",
+                           {required: "Achternaam mag niet leeg zijn"})}
+                />
+            </label>
+            {errors.achternaam && <p>{errors.achternaam.message}</p>}
+
+
+            <label>
+                Leeftijd:
+                <input type="number" placeholder="Leeftijd"
+                       {...register(
+                           "leeftijd",
+                           {required: true,
+                               min: {
+                                   value: 18,
+                                   message: "Minimaal 18 jaar oud.",
+                               }
                            })}
-            />
+                />
+            </label>
             {errors.leeftijd && <p>{errors.leeftijd.message}</p>}
 
-            <input type="text" placeholder="Postcode"
-                   {...register("postcode", {required: true, maxLength: 6, pattern: /[0-9]{4}[a-z-A-Z]{2}/i})}
-            />
-            <select {...register("bezorgfrequentie", { required: true })}>
-                <option value="Iedere week">Iedere week</option>
-                <option value="Om de week">Om de week</option>
-                <option value="Iedere maand">Iedere maand</option>
-                <option value="Anders">Anders</option>
+            <label>
+                Postcode:
+                <input type="text" placeholder="Postcode"
+                       {...register(
+                           "postcode",
+                           {required: "Postcode mag niet leeg zijn",
+                               minLength: {
+                               value: 6,
+                               message: "Postcode bestaat uit minimaal 6 tekens",
+                               },
+                               maxLength: {
+                               value: 6,
+                               message: "Postcode bestaat uit maximaal 6 tekens",
+                               },
+                               pattern: {
+                               value: /[0-9]{4}[a-z-A-Z]{2}/i ,
+                               message: "Postcode patroon: getal-getal-getal-getal-letter-letter",
+                               }
+                                   })}
+                />
+            </label>
+            {errors.postcode && <p>{errors.postcode.message}</p>}
 
-            </select>
+            <label>
+                Bezorgfrequentie:
+                <select {...register("bezorgfrequentie", { required: true })}>
+                    <option value="iedere week">Iedere week</option>
+                    <option value="om de week">Om de week</option>
+                    <option value="iedere maand">Iedere maand</option>
+                    <option value="anders">Anders</option>
+                </select>
+            </label>
+            {errors.bezorgfrequentie && <p>{errors.bezorgfrequentie.message}</p>}
+
             { chosenFreq === 'anders' && (
-            <input type="text" placeholder="Andere frequentie" {...register("andere frequentie", {})} />
+                <label>
+                    Voer een andere frequentie in:
+                    <input type="text" placeholder="Andere frequentie"
+                           {...register("anders", {})} />
+                </label>
             )}
-            <textarea {...register("opmerkingen", {})} />
-            <input type="checkbox" placeholder="Akkoord?" {...register("akkoord?", {required: true})} />
+
+            <label>
+                Opmerkingen:
+                <textarea
+                    {...register("opmerkingen", {})} />
+            </label>
+
+            <label>
+                Akkoord met de voorwaarden?
+                <input type="checkbox" placeholder="Akkoord?"
+                       {...register(
+                           "akkoord",
+                           {required: "Geen feestje zonder voorwaarden"}
+                       )}
+                />
+            </label>
+            {errors.akkoord && <p>{errors.akkoord.message}</p>}
+
 
             <input type="submit" />
         </form>
